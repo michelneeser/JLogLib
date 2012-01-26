2fb9f62c4a1e0d7096d10d0efda349b878af5e93
Michel Neeser <michel.neeser@gmx.ch>
2012-01-26 21:58:45 +0100

    init

---
+++ src/org/miness/jloglib/LogDateFormat.java
@@ -0,0 +1,43 @@
+/*
+ This file is part of JLogLib.
+
+ JLogLib is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ JLogLib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with JLogLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.miness.jloglib;
+
+/**
+ * Represents the three log date formats SHORT, MEDIUM and LONG.
+ * 
+ * @author Michel Neeser
+ * @version 1.0
+ */
+public enum LogDateFormat {
+	
+	/**
+	 * The enum constant which represents the short date format.
+	 */
+	SHORT,
+	
+	/**
+	 * The enum constant which represents the medium date format.
+	 */
+	MEDIUM,
+	
+	/**
+	 * The enum constant which represents the long date format.
+	 */
+	LONG
+	
+}
---
+++ src/org/miness/jloglib/LogLevel.java
@@ -0,0 +1,43 @@
+/*
+ This file is part of JLogLib.
+
+ JLogLib is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ JLogLib is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with JLogLib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.miness.jloglib;
+
+/**
+ * Represents the three log levels LOW, MEDIUM and HIGH.
+ * 
+ * @author Michel Neeser
+ * @version 1.0
+ */
+public enum LogLevel {
+
+	/**
+	 * The enum constant which represents the low log level.
+	 */
+	LOW,
+	
+	/**
+	 * The enum constant which represents the medium log level.
+	 */
+	MEDIUM,
+	
+	/**
+	 * The enum constant which represents the high log level.
+	 */
+	HIGH
+	
+}
